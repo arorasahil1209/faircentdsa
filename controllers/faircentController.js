ecd0ba176d2ac0fde1809c5877c6cb44844c6f38
sahil1219 <sahil_darora@hotmail.com>
2023-01-23 01:08:16 +0530

    dsa functionality

--- controllers/faircentController.js
+++ controllers/faircentController.js
@@ -150,34 +150,50 @@ let verifyOtp = async (req, res) => {
   }
 };
 
-let uploadDocument = async (req, res) => {
-  try {
-    console.log('body:::',req.fileKey);
+let uploadS3Docs = async (req, res) => {
+  try{
+        
+    console.log('dirname::',`${path.resolve('uploads')}/testdoc.png`)
+    console.log('files;:',req.file.originalname)
+   
+   console.log('req.file.buffer',req.file)
+    let datas = new FormData();
+    datas.append('type', req.body.type);
+    datas.append('uid', req.body.uid);
+    datas.append('fileKey', fs.createReadStream(`${path.resolve('uploads')}/${req.file.originalname}`));
     
-    // let data = new FormData();
-    // data.append('type', 'TEST');
-    // data.append('uid', '1234');
-    // data.append('fileKey', fs.createReadStream('/C:/Users/Sahil Arora/Desktop/testdoc.png'));
-    // var config = {
-    //   method: 'post',
-    //   url: 'https://fcnode11.faircent.com/v1/api/standard/upload',
-    //   headers: { 
-    //     'Content-Type': 'application/json', 
-    //     'x-application-id': 'b6419f1a493a09e21aae9de583f0d9dd', 
-    //     'x-application-name': 'FAIRCENT', 
-    //     ...data.getHeaders()
-    //   },
-    //   data : data
-    // };
-    // let uploadS3 = await httpReqequest(config);
-    // console.log('uplod s3::',uploadS3)
-  } catch (err) {
-    console.log("error::", err);
-    return res.json({
-      message: "Error occured",
-      error: err,
-    });
-  }
+    var config = {
+      method: 'post',
+      url: 'https://fcnode11.faircent.com/v1/api/standard/upload',
+      headers: { 
+        'Content-Type': 'application/json', 
+        'x-application-id': 'b6419f1a493a09e21aae9de583f0d9dd', 
+        'x-application-name': 'FAIRCENT',
+        ...datas.getHeaders()
+      },
+      data : datas
+    };
+    let s3Upload= await axios(config);
+    console.log('s3Upload::',s3Upload.data);
+    let userData = {
+        uid:req.body.uid,
+        photoId:s3Upload.data.result.fileKey
+    }
+    console.log("userData::::",userData);
+    let updateUserPhotograph =  await uploadUserPhoto(userData)
+    await fs.unlinkSync(`${path.resolve('uploads')}/${req.file.originalname}`);
+    return res.status(200).json({
+        message:'file uploaded successfully on s3 bucket',
+        data:s3Upload.data
+    })
+}catch(err){
+    console.log('s3 error',err)
+    return res.status(400).json({
+        message:'error occured while uploading data to s3',
+        error:err
+    })
+}
+    
 };
 
 
@@ -187,5 +203,6 @@ module.exports = {
   verifyPan,
   sendOtp,
   verifyOtp,
-  uploadDocument
+  uploadDocument,
+  uploadS3Docs
 };
--- routes/faircentRoutes.js
+++ routes/faircentRoutes.js
@@ -6,12 +6,10 @@ let path = require('path');
 let { uploadUserPhoto } = require("../controllers/leadControllers");
 let FormData = require('form-data');
 const multer = require("multer");
-//const upload = multer({ dest: './uploads/' });
+
 let router = express.Router();
-let {verifyPan,sendOtp,verifyOtp,uploadDocument
+let {verifyPan,sendOtp,verifyOtp,uploadS3Docs
     } = require('../controllers/faircentController');
-
-
 
 router.post('/pan-verification',verifyPan);
 router.post('/send-verification',sendOtp);
@@ -25,57 +23,53 @@ const storage= multer.diskStorage({
     }
   })
 
-//const storage= multer.memoryStorage()
-
-// const multerupload =upload.fields([
-//     {name:'fileKey',maxCount:1}
-// ])
 const upload =multer({storage})
-router.post('/upload-document',upload.single('fileKey'),async(req,res)=>{
-    try{
+router.post('/upload-document',upload.single('fileKey'),uploadS3Docs)
+// router.post('/upload-document',upload.single('fileKey'),async(req,res)=>{
+//     try{
         
-    console.log('dirname::',`${path.resolve('uploads')}/testdoc.png`)
-    console.log('files;:',req.file.originalname)
+//     console.log('dirname::',`${path.resolve('uploads')}/testdoc.png`)
+//     console.log('files;:',req.file.originalname)
    
-   console.log('req.file.buffer',req.file)
-    let datas = new FormData();
-    datas.append('type', req.body.type);
-    datas.append('uid', req.body.uid);
-    datas.append('fileKey', fs.createReadStream(`${path.resolve('uploads')}/${req.file.originalname}`));
+//    console.log('req.file.buffer',req.file)
+//     let datas = new FormData();
+//     datas.append('type', req.body.type);
+//     datas.append('uid', req.body.uid);
+//     datas.append('fileKey', fs.createReadStream(`${path.resolve('uploads')}/${req.file.originalname}`));
     
-    var config = {
-      method: 'post',
-      url: 'https://fcnode11.faircent.com/v1/api/standard/upload',
-      headers: { 
-        'Content-Type': 'application/json', 
-        'x-application-id': 'b6419f1a493a09e21aae9de583f0d9dd', 
-        'x-application-name': 'FAIRCENT',
-        ...datas.getHeaders()
-      },
-      data : datas
-    };
-    let s3Upload= await axios(config);
-    console.log('s3Upload::',s3Upload.data);
-    let userData = {
-        uid:req.body.uid,
-        photoId:s3Upload.data.result.fileKey
-    }
-    console.log("userData::::",userData);
-    let updateUserPhotograph =  await uploadUserPhoto(userData)
-    await fs.unlinkSync(`${path.resolve('uploads')}/${req.file.originalname}`);
-    return res.status(200).json({
-        message:'file uploaded successfully on s3 bucket',
-        data:s3Upload.data
+//     var config = {
+//       method: 'post',
+//       url: 'https://fcnode11.faircent.com/v1/api/standard/upload',
+//       headers: { 
+//         'Content-Type': 'application/json', 
+//         'x-application-id': 'b6419f1a493a09e21aae9de583f0d9dd', 
+//         'x-application-name': 'FAIRCENT',
+//         ...datas.getHeaders()
+//       },
+//       data : datas
+//     };
+//     let s3Upload= await axios(config);
+//     console.log('s3Upload::',s3Upload.data);
+//     let userData = {
+//         uid:req.body.uid,
+//         photoId:s3Upload.data.result.fileKey
+//     }
+//     console.log("userData::::",userData);
+//     let updateUserPhotograph =  await uploadUserPhoto(userData)
+//     await fs.unlinkSync(`${path.resolve('uploads')}/${req.file.originalname}`);
+//     return res.status(200).json({
+//         message:'file uploaded successfully on s3 bucket',
+//         data:s3Upload.data
 
-    })
-}catch(err){
-    console.log('s3 error',err)
-    return res.status(400).json({
-        message:'error occured while uploading data to s3',
-        error:err
-    })
-}
-});
+//     })
+// }catch(err){
+//     console.log('s3 error',err)
+//     return res.status(400).json({
+//         message:'error occured while uploading data to s3',
+//         error:err
+//     })
+// }
+// });
 
 
 
